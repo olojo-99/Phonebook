d8eca4436ebf26b6739b5e60933f701170b231c0
Przemyslaw Majda <przemyslaw.majda2@gmail.com>
2022-10-26 17:48:29 +0100

    fixed name search

--- procedural.c
+++ procedural.c
@@ -528,7 +528,7 @@ struct tree_node **test(struct tree_node *nametree, struct tree_node *numtree, E
     }
 
     // print_tree(nametree);
-    num_search(numtree, "456");
+    num_search(numtree, "123");
     print_tree(numtree);
     
     pointer_array[0] = nametree;
