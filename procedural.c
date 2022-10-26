db653646c73c7c0bcfa96cf98cf7b374cbb6467f
Przemyslaw Majda <przemyslaw.majda2@mail.dcu.ie>
2022-10-26 10:23:56 +0100

    working on testing

--- procedural.c
+++ procedural.c
@@ -519,9 +519,9 @@ void test(struct tree_node *nametree, struct tree_node *numtree, Entry e) {
         }
 
         j++;
-        printf("name1: %s\n\n", names[0]);
-        printf("num1: %s\n\n", nums[0]);
-        printf("add1: %s\n\n", addresses[0]);
+        printf("name1: %s\n\n", names[1]);
+        printf("num1: %s\n\n", nums[1]);
+        printf("add1: %s\n\n", addresses[1]);
         // found issue it overwrite name
     }
     
