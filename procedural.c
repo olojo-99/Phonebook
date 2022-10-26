efa5740a68887011c90e3a6017a7da6ec408799b
Przemyslaw Majda <przemyslaw.majda2@gmail.com>
2022-10-26 17:03:46 +0100

    ironing out bugs

--- procedural.c
+++ procedural.c
@@ -130,7 +130,7 @@ int main(void)
             // scanf("%c", &temp); // temp statement to clear buffer
             fflush(stdin);
             printf("Please enter the number: ");             
-            scanf("%d[^\n]", num);
+            scanf("%s[^\n]", num);
 
             /*Search for a node.*/            
             //num_search(pnum, num);         
